1f6f75013f1eae82210496d00a844e0fd544b88c
thoraya elsharawy <thorayaelsharawy@gmail.com>
2024-07-15 18:45:57 +0300

    Post feedback comment to server

--- src/App.tsx
+++ src/App.tsx
@@ -9,49 +9,60 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState('');
 
-  const handleAddFeedback = (text: string) => {
+  const handleAddFeedback = async (text: string) => {
     const companyName = text.split(' ').find(word => word.includes('#'))!.substring(1)
 
     const newItem: TFeedbackItem = {
-      id: Math.floor(Math.random() * 10),
+      id: new Date().getTime(),
       upvoteCount: 0,
       badgeLetter: companyName.substring(0, 1).toUpperCase(),
-      companyName: companyName,
+      company: companyName,
       text: text,
       daysAgo: 0
     }
-
     setFeedbackItems([...feedbackItems, newItem])
+
+  const res =  await fetch("https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks", {
+    method: 'POST',
+    body: JSON.stringify(newItem),
+    headers: {
+      Accept: "application/json" , 
+      "Content-Type": "application/json"
+    }
+  })
+
+  console.log(res)
+}
+
+useEffect(() => {
+  // console.log('here')
+
+  const fetchFeedBack = async () => {
+    setIsLoading(true)
+    try {
+      const res = await fetch("https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks")
+      if (!res.ok) {
+        throw new Error('Something went wrong')
+      }
+      const data = await res.json()
+      console.log(data)
+      setFeedbackItems(data.feedbacks)
+    } catch (error) {
+      setError('Something went wrong')
+    }
+    setIsLoading(false)
   }
 
-  useEffect(() => {
-    // console.log('here')
+  fetchFeedBack()
+}, [])
 
-    const fetchFeedBack = async () => {
-      setIsLoading(true)
-      try {
-        const res = await fetch("https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks")
-        if (!res.ok) {
-          throw new Error('Something went wrong')
-        }
-        const data = await res.json()
-        setFeedbackItems(data.feedbacks)
-      } catch (error) {
-        setError('Something went wrong')
-      }
-      setIsLoading(false)
-    }
-
-    fetchFeedBack()
-  }, [])
-
-  return (
-    <div className="app">
-      <Footer />
-      <Container feedbackItems={feedbackItems} isLoading={isLoading} error={error} handleAddFeedback={handleAddFeedback} />
-      <HashtagList />
-    </div>
-  )
+return (
+  <div className="app">
+    <Footer />
+    <Container feedbackItems={feedbackItems} isLoading={isLoading} error={error} handleAddFeedback={handleAddFeedback} />
+    <HashtagList />
+  </div>
+)
 }
 
 export default App

e1a399b75fc732ad4930aec9bc16c6254936d5ba
thoraya elsharawy <thorayaelsharawy@gmail.com>
2024-07-15 10:41:09 +0300

    Add Feedback form strucure and validation

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,18 +1,32 @@
+import { useState } from "react"
+import { MAX_CHARACTERS } from "../lib/constants";
+
 export default function FeedbackForm() {
+    const [text, setText] = useState("")
+    const charCount = MAX_CHARACTERS - text.length;
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const newText = e.target.value
+        if (newText.length > MAX_CHARACTERS) return
+        setText(newText)
+    }
+
     return <form className="form">
         <textarea
+            value={text}
+            onChange={handleChange}
             id="feedback-textarea"
             placeholder="bla"
             spellCheck={false}
-            maxLength={150}
+            maxLength={MAX_CHARACTERS}
         />
-        
+
         <label htmlFor="feedback-textarea">
             Enter your feedback here, remember to #hashtag the company
         </label>
 
         <div>
-            <p className="u-italic">150</p>
+            <p className="u-italic">{charCount}</p>
             <button>
                 <span>Submit</span>
             </button>

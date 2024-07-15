0e5e0723060184f5ce91722e4170f2545575a433
thoraya elsharawy <thorayaelsharawy@gmail.com>
2024-07-15 22:40:13 +0300

    upvote count in feedback item

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -1,17 +1,26 @@
 import { GoTriangleUp } from "react-icons/go";
-import { type TFeedbackItem } from "../../lib/types"; 
+import { type TFeedbackItem } from "../../lib/types";
+import { useState } from "react";
 
 type FeedbackItemProps = {
     feedbackItem: TFeedbackItem
 }
 
 export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
-    return (
-        <li className="feedback">
-            <button>
-                <GoTriangleUp />
+    const [open, setOpen] = useState(false);
+    const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount)
 
-                <span>{feedbackItem.upvoteCount}</span>
+    const handleUpvote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        setUpvoteCount(prev => prev + 1);
+        e.currentTarget.disabled = true
+        e.stopPropagation()
+    }
+
+    return (
+        <li onClick={() => setOpen(prev => !prev)} className={`feedback ${open ? 'feedback--expand' : ''}`}>
+            <button onClick={handleUpvote}>
+                <GoTriangleUp />
+                <span>{upvoteCount}</span>
             </button>
             <div>
                 <p>{feedbackItem.badgeLetter}</p>
